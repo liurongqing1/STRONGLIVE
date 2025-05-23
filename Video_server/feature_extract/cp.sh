61d18fb6893d3989d47abd00cd68f86459cc44c5
liurongqing1 <106973901+liurongqing1@users.noreply.github.com>
2025-05-23 18:24:04 +0800

    Create cp.sh

---
+++ Video_server/feature_extract/cp.sh
@@ -0,0 +1,38 @@
+mkdir /home/srteam/lrq/feature_extract/GT/Jockey_014
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/Jockey_014/HR/*  /home/srteam/lrq/feature_extract/GT/Jockey_014
+mkdir /home/srteam/lrq/feature_extract/GT/Jockey_015
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/Jockey_014/HR/*  /home/srteam/lrq/feature_extract/GT/Jockey_015
+
+mkdir /home/srteam/lrq/feature_extract/GT/mulan_000
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/mulan_000/HR/*  /home/srteam/lrq/feature_extract/GT/mulan_000
+mkdir /home/srteam/lrq/feature_extract/GT/mulan_001
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/mulan_001/HR/*  /home/srteam/lrq/feature_extract/GT/mulan_001
+mkdir /home/srteam/lrq/feature_extract/GT/mulan_016
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/mulan_016/HR/*  /home/srteam/lrq/feature_extract/GT/mulan_016
+
+mkdir /home/srteam/lrq/feature_extract/GT/surf_000
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/surf_000/HR/*  /home/srteam/lrq/feature_extract/GT/surf_000
+mkdir /home/srteam/lrq/feature_extract/GT/surf_001
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/surf_001/HR/*  /home/srteam/lrq/feature_extract/GT/surf_001
+mkdir /home/srteam/lrq/feature_extract/GT/surf_002
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/surf_002/HR/*  /home/srteam/lrq/feature_extract/GT/surf_002
+mkdir /home/srteam/lrq/feature_extract/GT/surf_003
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/surf_003/HR/*  /home/srteam/lrq/feature_extract/GT/surf_003
+mkdir /home/srteam/lrq/feature_extract/GT/surf_004
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/surf_004/HR/*  /home/srteam/lrq/feature_extract/GT/surf_004
+mkdir /home/srteam/lrq/feature_extract/GT/surf_005
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/surf_005/HR/*  /home/srteam/lrq/feature_extract/GT/surf_005
+mkdir /home/srteam/lrq/feature_extract/GT/surf_006
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/surf_006/HR/*  /home/srteam/lrq/feature_extract/GT/surf_006
+mkdir /home/srteam/lrq/feature_extract/GT/surf_007
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/surf_007/HR/*  /home/srteam/lrq/feature_extract/GT/surf_007
+mkdir /home/srteam/lrq/feature_extract/GT/surf_008
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/surf_008/HR/*  /home/srteam/lrq/feature_extract/GT/surf_008
+mkdir /home/srteam/lrq/feature_extract/GT/surf_009
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/surf_009/HR/*  /home/srteam/lrq/feature_extract/GT/surf_009
+mkdir /home/srteam/lrq/feature_extract/GT/surf_010
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/surf_010/HR/*  /home/srteam/lrq/feature_extract/GT/surf_010
+mkdir /home/srteam/lrq/feature_extract/GT/surf_011
+cp -r /home/srteam/lrq/EDSR-PyTorch/dataset/benchmark/surf_011/HR/*  /home/srteam/lrq/feature_extract/GT/surf_011
+
+
